ddcb972b80c17fdd6c5ea922f79cc0c0f4f39158
h8windows <h8windows@gmail.com>
2015-02-08 17:54:24 -0800

    rolled back to tut

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome_page/welcome'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'tasks#index'
-  root 'welcome_page#welcome'
+  root 'tasks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
